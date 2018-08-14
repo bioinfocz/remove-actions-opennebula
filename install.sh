df1e0fab24e7ec051a4c15541f186e33391340b8
Jiri Novotny <fg-42@seznam.cz>
2018-08-14 12:19:23 +0200

    fixed installation dependencies

--- install.sh
+++ install.sh
@@ -1,7 +1,16 @@
 #!/usr/bin/env bash
 
+sudo apt install curl libnss3
+
 CHROME_DRIVER_VERSION=$(curl -sS chromedriver.storage.googleapis.com/LATEST_RELEASE)
 
+# Install chrome
+sudo curl -sS -o - https://dl-ssl.google.com/linux/linux_signing_key.pub | apt-key add
+sudo echo "deb http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google-chrome.list
+sudo apt-get -y update
+sudo apt-get -y install google-chrome-stable
+
+# Install chromedriver
 wget -N http://chromedriver.storage.googleapis.com/${CHROME_DRIVER_VERSION}/chromedriver_linux64.zip
 unzip -o chromedriver_linux64.zip
 rm chromedriver_linux64.zip
@@ -13,7 +22,7 @@ python setup.py install
 
 if [[ $? == 0 ]]; then
     echo
-    echo "$(tput setaf 2)Script successfully installed. Now you can use it from command-line as $(tput setaf 5)remove_actions_vm$(tput setaf 2)"
+    echo "$(tput setaf 2)Script successfully installed. Now you can use it from command-line as $(tput setaf 5)remove_actions_opennebula$(tput setaf 2)"
 fi
 
-python setup.py clean --all
+tput init
